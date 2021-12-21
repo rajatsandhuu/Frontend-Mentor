a1b02354770085246f53c5a056680fcaac93b8ff
rajatsinghsandhu <81261270+rajatsinghsandhu@users.noreply.github.com>
2021-12-21 11:10:44 +0530

    updated

---
+++ Calculator App/README.md
@@ -0,0 +1,44 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
+
+### Links
+
+- Solution URL: [Solution](https://github.com/rajatsinghsandhu/Frontend-Mentor/tree/main/Calculator%20App)
+- Live Site URL: [Calculator App](https://rajatsinghsandhu.github.io/Frontend-Mentor/Calculator%20App)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JavaScript
+
+## Author
+
+- Github Profile - [Rajat](https://github.com/rajatsinghsandhu)
+- Frontend Mentor - [@rajatsinghsandhu](https://www.frontendmentor.io/profile/rajatsinghsandhu)
---
+++ Calculator App/script.js
@@ -0,0 +1,136 @@
+const circle = document.querySelector(".circle");
+const themeOne = document.querySelector(".theme-1");
+const themeTwo = document.querySelector(".theme-2");
+const themeThree = document.querySelector(".theme-3");
+const r = document.documentElement.dataset;
+const gridButtons = document.querySelectorAll(".keypad-grid button");
+const deleteButton = document.querySelector("#delete-btn");
+const resetButton = document.querySelector("#reset-btn");
+const equalToButton = document.querySelector("#equal-to-btn");
+const currentOperand = document.querySelector(".current-operand");
+const previousOperand = document.querySelector(".previous-operand");
+
+let firstOperand, secondOperand, finalResult, operator;
+let regex = new RegExp(",", "g");
+
+themeOne.addEventListener("click", function () {
+  circle.style.transform = "translateX(0px)";
+  document.body.removeAttribute("data-theme");
+});
+
+themeTwo.addEventListener("click", function () {
+  circle.style.transform = "translateX(24px)";
+  document.body.setAttribute("data-theme", "theme-two");
+});
+
+themeThree.addEventListener("click", function () {
+  circle.style.transform = "translateX(48px)";
+  document.body.setAttribute("data-theme", "theme-three");
+});
+
+for (let i = 0; i < gridButtons.length; i++) {
+  gridButtons[i].addEventListener("click", function () {
+    if (
+      gridButtons[i].textContent !== "DEL" &&
+      gridButtons[i].textContent !== "RESET" &&
+      gridButtons[i].textContent !== "="
+    ) {
+      if (currentOperand.textContent.includes(".")) {
+        if (gridButtons[i].textContent !== ".") {
+          currentOperand.textContent += gridButtons[i].textContent;
+          if (
+            !currentOperand.textContent.includes(".") &&
+            !currentOperand.textContent.includes("+") &&
+            !currentOperand.textContent.includes("-") &&
+            !currentOperand.textContent.includes("*") &&
+            !currentOperand.textContent.includes("/")
+          ) {
+            currentOperand.textContent = parseFloat(
+              currentOperand.textContent.replace(regex, "")
+            ).toLocaleString();
+          }
+        }
+      } else {
+        currentOperand.textContent += gridButtons[i].textContent;
+        if (
+          !currentOperand.textContent.includes(".") &&
+          !currentOperand.textContent.includes("+") &&
+          !currentOperand.textContent.includes("-") &&
+          !currentOperand.textContent.includes("*") &&
+          !currentOperand.textContent.includes("/")
+        ) {
+          currentOperand.textContent = parseFloat(
+            currentOperand.textContent.replace(regex, "")
+          ).toLocaleString();
+        }
+      }
+    }
+    if (
+      gridButtons[i].textContent === "+" ||
+      gridButtons[i].textContent === "-" ||
+      gridButtons[i].textContent === "x" ||
+      gridButtons[i].textContent === "/"
+    ) {
+      if (currentOperand.innerHTML.trim().length !== 1) {
+        operator = gridButtons[i].textContent;
+        firstOperand = parseFloat(
+          currentOperand.textContent.replace(regex, "")
+        );
+        console.log(firstOperand);
+        previousOperand.textContent = currentOperand.textContent;
+        currentOperand.textContent = "";
+      } else if (currentOperand.innerHTML.trim().length === 1) {
+        if (
+          gridButtons[i].textContent === "x" ||
+          gridButtons[i].textContent === "/"
+        ) {
+          operator = gridButtons[i].textContent;
+          firstOperand = parseFloat(
+            currentOperand.textContent.replace(regex, "")
+          );
+          previousOperand.textContent = currentOperand.textContent;
+          currentOperand.textContent = "";
+        }
+      }
+    }
+  });
+}
+
+deleteButton.addEventListener("click", function () {
+  currentOperand.textContent = currentOperand.textContent.slice(0, -1);
+});
+
+resetButton.addEventListener("click", function () {
+  previousOperand.textContent = "";
+  currentOperand.textContent = "";
+  firstOperand = undefined;
+  secondOperand = undefined;
+  operator = undefined;
+  finalResult = undefined;
+});
+
+equalToButton.addEventListener("click", function () {
+  previousOperand.textContent = "";
+  secondOperand = parseFloat(currentOperand.textContent.replace(regex, ""));
+  console.log(secondOperand);
+  switch (operator) {
+    case "+":
+      finalResult = firstOperand + secondOperand;
+      break;
+    case "-":
+      finalResult = firstOperand - secondOperand;
+      break;
+    case "x":
+      finalResult = firstOperand * secondOperand;
+      break;
+    case "/":
+      finalResult = firstOperand / secondOperand;
+      break;
+  }
+  if (
+    typeof firstOperand !== "undefined" &&
+    typeof secondOperand !== "undefined"
+  ) {
+    currentOperand.textContent = finalResult.toLocaleString();
+  }
+});
