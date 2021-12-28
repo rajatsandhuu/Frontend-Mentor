c373cf4b6fcba5290bab11d77fffec44579eedf2
rajatsinghsandhu <81261270+rajatsinghsandhu@users.noreply.github.com>
2021-12-28 14:42:37 +0530

    updated

---
+++ E-commerce Product Page/README.md
@@ -0,0 +1,45 @@
+# Frontend Mentor - E-commerce product page solution
+
+This is a solution to the [E-commerce product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
+
+### Links
+
+- Solution URL: [Solution](https://github.com/rajatsinghsandhu/Frontend-Mentor/tree/main/E-commerce%20Product%20Page)
+- Live Site URL: [E-commerce Product Page](https://rajatsinghsandhu.github.io/Frontend-Mentor/E-commerce%20Product%20Page)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- JavaScript
+
+## Author
+
+- Github Profile - [Rajat](https://github.com/rajatsinghsandhu)
+- Frontend Mentor - [@rajatsinghsandhu](https://www.frontendmentor.io/profile/rajatsinghsandhu)
---
+++ E-commerce Product Page/script.js
@@ -0,0 +1,271 @@
+const cartButton = document.querySelector(".cart-image");
+const cartContainer = document.querySelector(".cart-container");
+const prevSymbol = document.querySelector(
+  ".product-image-container .previous-symbol"
+);
+const nextSymbol = document.querySelector(
+  ".product-image-container .next-symbol"
+);
+const lightboxPrevSymbol = document.querySelector(".lightbox .previous-symbol");
+const lightboxNextSymbol = document.querySelector(".lightbox .next-symbol");
+const productImage = document.getElementById("product-img");
+const lightboxProductImage = document.getElementById("lightbox-product-img");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navLinks = document.querySelector(".nav-links");
+const minusSymbol = document.querySelector(".minus");
+const plusSymbol = document.querySelector(".plus");
+const quantity = document.querySelector(".quantity");
+const addToCartBtn = document.querySelector(".add-to-cart-btn");
+const deleteIcon = document.querySelector(".delete-icon");
+const thumbnailOne = document.querySelector(".thumbnail-1");
+const thumbnailTwo = document.querySelector(".thumbnail-2");
+const thumbnailThree = document.querySelector(".thumbnail-3");
+const thumbnailFour = document.querySelector(".thumbnail-4");
+const lightboxThumbnailOne = document.querySelector(".lightbox-thumbnail-1");
+const lightboxThumbnailTwo = document.querySelector(".lightbox-thumbnail-2");
+const lightboxThumbnailThree = document.querySelector(".lightbox-thumbnail-3");
+const lightboxThumbnailFour = document.querySelector(".lightbox-thumbnail-4");
+const closeLightbox = document.querySelector(".close-lightbox");
+
+cartButton.addEventListener("click", function () {
+  if (cartContainer.style.display === "none") {
+    cartButton.classList.add("cart-active");
+    cartContainer.style.display = "block";
+  } else {
+    cartButton.classList.remove("cart-active");
+    cartContainer.style.display = "none";
+  }
+});
+
+prevSymbol.addEventListener("click", function () {
+  if (productImage.getAttribute("src") === "images/image-product-1.jpg") {
+    productImage.src = "images/image-product-4.jpg";
+  } else if (
+    productImage.getAttribute("src") === "images/image-product-2.jpg"
+  ) {
+    productImage.src = "images/image-product-1.jpg";
+  } else if (
+    productImage.getAttribute("src") === "images/image-product-3.jpg"
+  ) {
+    productImage.src = "images/image-product-2.jpg";
+  } else if (
+    productImage.getAttribute("src") === "images/image-product-4.jpg"
+  ) {
+    productImage.src = "images/image-product-3.jpg";
+  }
+});
+
+nextSymbol.addEventListener("click", function () {
+  if (productImage.getAttribute("src") === "images/image-product-1.jpg") {
+    productImage.src = "images/image-product-2.jpg";
+  } else if (
+    productImage.getAttribute("src") === "images/image-product-2.jpg"
+  ) {
+    productImage.src = "images/image-product-3.jpg";
+  } else if (
+    productImage.getAttribute("src") === "images/image-product-3.jpg"
+  ) {
+    productImage.src = "images/image-product-4.jpg";
+  } else if (
+    productImage.getAttribute("src") === "images/image-product-4.jpg"
+  ) {
+    productImage.src = "images/image-product-1.jpg";
+  }
+});
+
+hamburgerMenu.addEventListener("click", function () {
+  if (!document.querySelector(".line-1").classList.contains("line-1-active")) {
+    navLinks.style.display = "block";
+    document.querySelector(".line-1").classList.add("line-1-active");
+    document.querySelector(".line-2").classList.add("line-2-active");
+    document.querySelector(".line-3").classList.add("line-3-active");
+    document.body.style.overflowY = "hidden";
+  } else {
+    navLinks.style.display = "none";
+    document.querySelector(".line-1").classList.remove("line-1-active");
+    document.querySelector(".line-2").classList.remove("line-2-active");
+    document.querySelector(".line-3").classList.remove("line-3-active");
+    document.body.style.overflowY = "visible";
+  }
+});
+
+minusSymbol.addEventListener("click", function () {
+  if (quantity.textContent > 1) {
+    quantity.textContent--;
+  }
+});
+
+plusSymbol.addEventListener("click", function () {
+  quantity.textContent++;
+});
+
+addToCartBtn.addEventListener("click", function () {
+  document.querySelector(".cart-quantity").style.display = "inline-block";
+  document.querySelector(".cart-quantity").textContent = quantity.textContent;
+  document.querySelector(".cart-is-empty").style.display = "none";
+  document.querySelector(".cart-container h4").classList.remove("cart-title");
+  document.querySelector(".product-quantity").textContent =
+    quantity.textContent;
+  document.querySelector(".total-price").textContent = `$${(
+    125 * parseFloat(quantity.textContent)
+  ).toFixed(2)}`;
+  document.querySelector(".cart-with-checkout").style.display = "block";
+});
+
+deleteIcon.addEventListener("click", function () {
+  document.querySelector(".cart-quantity").style.display = "none";
+  document.querySelector(".cart-is-empty").style.display = "block";
+  document.querySelector(".cart-container h4").classList.add("cart-title");
+  document.querySelector(".cart-with-checkout").style.display = "none";
+});
+
+function run() {
+  const mediaQuery = window.matchMedia("(min-width: 1200px)");
+  var enableLightbox = function () {
+    document.body.classList.add("overlay");
+    document.querySelector(".lightbox").style.display = "block";
+    document.querySelector(".lightbox").style.position = "absolute";
+  };
+  function onMediaQueryChange() {
+    if (mediaQuery.matches) {
+      productImage.addEventListener("click", enableLightbox);
+
+      thumbnailOne.addEventListener("click", function () {
+        thumbnailOne.classList.add("active");
+        thumbnailTwo.classList.remove("active");
+        thumbnailThree.classList.remove("active");
+        thumbnailFour.classList.remove("active");
+        productImage.src = "images/image-product-1.jpg";
+      });
+
+      thumbnailTwo.addEventListener("click", function () {
+        thumbnailOne.classList.remove("active");
+        thumbnailTwo.classList.add("active");
+        thumbnailThree.classList.remove("active");
+        thumbnailFour.classList.remove("active");
+        productImage.src = "images/image-product-2.jpg";
+      });
+
+      thumbnailThree.addEventListener("click", function () {
+        thumbnailOne.classList.remove("active");
+        thumbnailTwo.classList.remove("active");
+        thumbnailThree.classList.add("active");
+        thumbnailFour.classList.remove("active");
+        productImage.src = "images/image-product-3.jpg";
+      });
+
+      thumbnailFour.addEventListener("click", function () {
+        thumbnailOne.classList.remove("active");
+        thumbnailTwo.classList.remove("active");
+        thumbnailThree.classList.remove("active");
+        thumbnailFour.classList.add("active");
+        productImage.src = "images/image-product-4.jpg";
+      });
+
+      lightboxThumbnailOne.addEventListener("click", function () {
+        lightboxThumbnailOne.classList.add("lightbox-active");
+        lightboxThumbnailTwo.classList.remove("lightbox-active");
+        lightboxThumbnailThree.classList.remove("lightbox-active");
+        lightboxThumbnailFour.classList.remove("lightbox-active");
+        lightboxProductImage.src = "images/image-product-1.jpg";
+      });
+
+      lightboxThumbnailTwo.addEventListener("click", function () {
+        lightboxThumbnailOne.classList.remove("lightbox-active");
+        lightboxThumbnailTwo.classList.add("lightbox-active");
+        lightboxThumbnailThree.classList.remove("lightbox-active");
+        lightboxThumbnailFour.classList.remove("lightbox-active");
+        lightboxProductImage.src = "images/image-product-2.jpg";
+      });
+
+      lightboxThumbnailThree.addEventListener("click", function () {
+        lightboxThumbnailOne.classList.remove("lightbox-active");
+        lightboxThumbnailTwo.classList.remove("lightbox-active");
+        lightboxThumbnailThree.classList.add("lightbox-active");
+        lightboxThumbnailFour.classList.remove("lightbox-active");
+        lightboxProductImage.src = "images/image-product-3.jpg";
+      });
+
+      lightboxThumbnailFour.addEventListener("click", function () {
+        lightboxThumbnailOne.classList.remove("lightbox-active");
+        lightboxThumbnailTwo.classList.remove("lightbox-active");
+        lightboxThumbnailThree.classList.remove("lightbox-active");
+        lightboxThumbnailFour.classList.add("lightbox-active");
+        lightboxProductImage.src = "images/image-product-4.jpg";
+      });
+
+      lightboxPrevSymbol.addEventListener("click", function () {
+        if (
+          lightboxProductImage.getAttribute("src") ===
+          "images/image-product-1.jpg"
+        ) {
+          lightboxProductImage.src = "images/image-product-4.jpg";
+          lightboxThumbnailOne.classList.remove("lightbox-active");
+          lightboxThumbnailFour.classList.add("lightbox-active");
+        } else if (
+          lightboxProductImage.getAttribute("src") ===
+          "images/image-product-2.jpg"
+        ) {
+          lightboxProductImage.src = "images/image-product-1.jpg";
+          lightboxThumbnailTwo.classList.remove("lightbox-active");
+          lightboxThumbnailOne.classList.add("lightbox-active");
+        } else if (
+          lightboxProductImage.getAttribute("src") ===
+          "images/image-product-3.jpg"
+        ) {
+          lightboxProductImage.src = "images/image-product-2.jpg";
+          lightboxThumbnailThree.classList.remove("lightbox-active");
+          lightboxThumbnailTwo.classList.add("lightbox-active");
+        } else if (
+          lightboxProductImage.getAttribute("src") ===
+          "images/image-product-4.jpg"
+        ) {
+          lightboxProductImage.src = "images/image-product-3.jpg";
+          lightboxThumbnailFour.classList.remove("lightbox-active");
+          lightboxThumbnailThree.classList.add("lightbox-active");
+        }
+      });
+
+      lightboxNextSymbol.addEventListener("click", function () {
+        if (
+          lightboxProductImage.getAttribute("src") ===
+          "images/image-product-1.jpg"
+        ) {
+          lightboxProductImage.src = "images/image-product-2.jpg";
+          lightboxThumbnailOne.classList.remove("lightbox-active");
+          lightboxThumbnailTwo.classList.add("lightbox-active");
+        } else if (
+          lightboxProductImage.getAttribute("src") ===
+          "images/image-product-2.jpg"
+        ) {
+          lightboxProductImage.src = "images/image-product-3.jpg";
+          lightboxThumbnailTwo.classList.remove("lightbox-active");
+          lightboxThumbnailThree.classList.add("lightbox-active");
+        } else if (
+          lightboxProductImage.getAttribute("src") ===
+          "images/image-product-3.jpg"
+        ) {
+          lightboxProductImage.src = "images/image-product-4.jpg";
+          lightboxThumbnailThree.classList.remove("lightbox-active");
+          lightboxThumbnailFour.classList.add("lightbox-active");
+        } else if (
+          lightboxProductImage.getAttribute("src") ===
+          "images/image-product-4.jpg"
+        ) {
+          lightboxProductImage.src = "images/image-product-1.jpg";
+          lightboxThumbnailFour.classList.remove("lightbox-active");
+          lightboxThumbnailOne.classList.add("lightbox-active");
+        }
+      });
+
+      closeLightbox.addEventListener("click", function () {
+        document.querySelector(".lightbox").style.display = "none";
+        document.body.classList.remove("overlay");
+      });
+    } else {
+      productImage.removeEventListener("click", enableLightbox);
+    }
+  }
+  mediaQuery.onchange = onMediaQueryChange;
+  onMediaQueryChange();
+}
