69c9f319701291439ddf1e84de982411074a5aa9
rajatsinghsandhu <81261270+rajatsinghsandhu@users.noreply.github.com>
2022-03-22 13:38:14 +0530

    Initial commit

---
+++ Interactive Rating Component/README.md
@@ -0,0 +1,42 @@
+# Frontend Mentor - Interactive rating component solution
+
+This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+
+### Links
+
+- Solution URL: [Solution](https://github.com/rajatsinghsandhu/Frontend-Mentor/tree/main/Interactive%20Rating%20Component)
+- Live Site URL: [Interactive Rating Component](https://rajatsinghsandhu.github.io/Frontend-Mentor/Interactive%20Rating%20Component)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS3
+- Flexbox
+- JavaScript
+
+## Author
+
+- Github Profile - [Rajat](https://github.com/rajatsinghsandhu)
+- Frontend Mentor - [@rajatsinghsandhu](https://www.frontendmentor.io/profile/rajatsinghsandhu)
---
+++ Interactive Rating Component/script.js
@@ -0,0 +1,78 @@
+const oneRating = document.querySelector('.one-rating');
+const twoRating = document.querySelector('.two-rating');
+const threeRating = document.querySelector('.three-rating');
+const fourRating = document.querySelector('.four-rating');
+const fiveRating = document.querySelector('.five-rating');
+const submitBtn = document.querySelector('.submit-btn');
+oneRating.addEventListener('click', () => {
+  if (!oneRating.classList.contains('active')) {
+    oneRating.classList.add('active');
+    twoRating.classList.remove('active');
+    threeRating.classList.remove('active');
+    fourRating.classList.remove('active');
+    fiveRating.classList.remove('active');
+  }
+});
+twoRating.addEventListener('click', () => {
+  if (!twoRating.classList.contains('active')) {
+    oneRating.classList.remove('active');
+    twoRating.classList.add('active');
+    threeRating.classList.remove('active');
+    fourRating.classList.remove('active');
+    fiveRating.classList.remove('active');
+  }
+});
+threeRating.addEventListener('click', () => {
+  if (!threeRating.classList.contains('active')) {
+    oneRating.classList.remove('active');
+    twoRating.classList.remove('active');
+    threeRating.classList.add('active');
+    fourRating.classList.remove('active');
+    fiveRating.classList.remove('active');
+  }
+});
+fourRating.addEventListener('click', () => {
+  if (!fourRating.classList.contains('active')) {
+    oneRating.classList.remove('active');
+    twoRating.classList.remove('active');
+    threeRating.classList.remove('active');
+    fourRating.classList.add('active');
+    fiveRating.classList.remove('active');
+  }
+});
+fiveRating.addEventListener('click', () => {
+  if (!fiveRating.classList.contains('active')) {
+    oneRating.classList.remove('active');
+    twoRating.classList.remove('active');
+    threeRating.classList.remove('active');
+    fourRating.classList.remove('active');
+    fiveRating.classList.add('active');
+  }
+});
+submitBtn.addEventListener('click', () => {
+  let numRating;
+  if (document.querySelector('.active')) {
+    document.querySelector('.home-page').classList.add('hide');
+    setTimeout(() => {
+      document.querySelector('.home-page').style.display = 'none';
+      document.querySelector('.thank-you-page').style.display = 'flex';
+    }, 500);
+    setTimeout(() => {
+      document.querySelector('.thank-you-page').classList.add('show');
+    }, 1000);
+    if (document.querySelector('.active').className.includes('one')) {
+      numRating = 1;
+    } else if (document.querySelector('.active').className.includes('two')) {
+      numRating = 2;
+    } else if (document.querySelector('.active').className.includes('three')) {
+      numRating = 3;
+    } else if (document.querySelector('.active').className.includes('four')) {
+      numRating = 4;
+    } else if (document.querySelector('.active').className.includes('five')) {
+      numRating = 5;
+    }
+    document.querySelector(
+      '.selected-rating-container'
+    ).textContent = `You selected ${numRating} out of 5`;
+  }
+});
