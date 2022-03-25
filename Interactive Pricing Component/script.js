705a9a05c2676801f144010a555a16d5910e9c1b
rajatsinghsandhu <81261270+rajatsinghsandhu@users.noreply.github.com>
2022-03-26 01:51:21 +0530

    Updated bill per year as well as pageviews

--- Interactive Pricing Component/script.js
+++ Interactive Pricing Component/script.js
@@ -4,31 +4,44 @@ const max = slider.max;
 const value = slider.value;
 const bill = document.querySelector('.bill');
 const checkbox = document.querySelector('.check-box');
+const pageViews = document.querySelector('.page-views');
 
 function updateBill() {
   if (checkbox.checked) {
+    document.querySelector('.per-month').textContent = '/ year';
     if (slider.value == '1') {
-      bill.textContent = '$6.00';
+      bill.textContent = '$72.00';
+      pageViews.textContent = '10K';
     } else if (slider.value == '2') {
-      bill.textContent = '$9.00';
+      bill.textContent = '$108.00';
+      pageViews.textContent = '50K';
     } else if (slider.value == '3') {
-      bill.textContent = '$12.00';
+      bill.textContent = '$144.00';
+      pageViews.textContent = '100K';
     } else if (slider.value == '4') {
-      bill.textContent = '$18.00';
+      bill.textContent = '$216.00';
+      pageViews.textContent = '500K';
     } else if (slider.value == '5') {
-      bill.textContent = '$27.00';
+      bill.textContent = '$324.00';
+      pageViews.textContent = '1M';
     }
   } else {
+    document.querySelector('.per-month').textContent = '/ month';
     if (slider.value == '1') {
       bill.textContent = '$8.00';
+      pageViews.textContent = '10K';
     } else if (slider.value == '2') {
       bill.textContent = '$12.00';
+      pageViews.textContent = '50K';
     } else if (slider.value == '3') {
       bill.textContent = '$16.00';
+      pageViews.textContent = '100K';
     } else if (slider.value == '4') {
       bill.textContent = '$24.00';
+      pageViews.textContent = '500K';
     } else if (slider.value == '5') {
       bill.textContent = '$36.00';
+      pageViews.textContent = '1M';
     }
   }
 }
